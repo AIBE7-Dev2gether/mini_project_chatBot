03e768bd9ee629941bd0c84e7c71adb0a28212f9
ImJhoon <jhjh9907@gmail.com>
2026-07-02 11:15:20 +0900

    refactor: 기본 서블릿 제거 및 BaseController 추가

---
+++ src/main/java/com/example/archat/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.example.archat.controller;
+
+import jakarta.servlet.http.HttpServlet;
+
+public abstract class BaseController extends HttpServlet {
+    static final String VIEW_PREFIX = "/WEB-INF/VIEWS";
+}
