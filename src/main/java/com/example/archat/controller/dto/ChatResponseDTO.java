739e799d5f5338c9e30dfe148f0b2ab8574e0444
ImJhoon <jhjh9907@gmail.com>
2026-07-02 12:01:29 +0900

    feat: ChatResponseDTO 추가 및 불필요한 index.jsp 삭제

---
+++ src/main/java/com/example/archat/controller/dto/ChatResponseDTO.java
@@ -0,0 +1,30 @@
+package com.example.archat.controller.dto;
+
+import com.example.archat.model.Chat;
+
+public record ChatResponseDTO(String owner, String model, String message, String timestamp) {
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public static ChatResponseDTO of(Chat chat) {
+        return new ChatResponseDTO(
+                chat.owner(),
+                chat.model(),
+                chat.message(),
+                chat.timestamp()
+        );
+    }
+}
